9b4cb2b690f7da57b78b601c0b4cefd3ba460c00
hymnken <hymnken@yeah.net>
2023-05-06 01:13:21 +0800

    页面标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,15 +6,18 @@ const routes = [
     path: '/',
     component: () => import('@/views/layout/index.vue'),
     children: [
-      { path: '/home', component: () => import('@/views/home/index.vue') },
-      { path: '/article', component: () => import('@/views/article/index.vue') },
-      { path: '/notify', component: () => import('@/views/notify/index.vue') },
-      { path: '/user', component: () => import('@/views/user/index.vue') },
+      { path: '/home', component: () => import('@/views/home/index.vue'), meta: { title: '首页' } },
+      { path: '/article', component: () => import('@/views/article/index.vue'), meta: { title: '健康百科' } },
+      { path: '/notify', component: () => import('@/views/notify/index.vue'), meta: { title: '消息通知' } },
+      { path: '/user', component: () => import('@/views/user/index.vue'), meta: { title: '个人中心' } },
     ],
   },
   {
     path: '/login',
     component: () => import('@/views/login/index.vue'),
+    meta: {
+      title: '登录',
+    },
   },
 ]
 
@@ -25,6 +28,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from) => {
+  document.title = `津医问诊-${to.meta.title}`
   const store = useUserStore()
   if (!store.user.token && !['login', '/other'].includes(to.path)) return '/login'
 })
