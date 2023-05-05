6e4aad95e277d847c5f69cc55bc1a2d99682adef
hymnken <hymnken@yeah.net>
2023-05-06 00:59:02 +0800

    创还能枚举常量

--- src/api/user.ts
+++ src/api/user.ts
@@ -10,17 +10,17 @@ export const sendMobileCode = (mobile: string, type: CodeType) => request('code'
 // 短信登录
 export const loginByMobile = (mobile: string, code: string) => request<User>('login', 'POST', { mobile, code })
 
-// // 获取用户信息
-// export const getUserInfo = () => request<UserInfo>('patient/myUser')
+// 获取用户信息
+export const getUserInfo = () => request<UserInfo>('patient/myUser')
 
-// // 获取患者列表信息
-// export const getPatientList = () => request<PatientList>('patient/mylist')
+// 获取患者列表信息
+export const getPatientList = () => request<PatientList>('patient/mylist')
 
-// // 添加患者
-// export const addPatient = (patient: Patient) => request('patient/add', 'POST', patient)
+// 添加患者
+export const addPatient = (patient: Patient) => request('patient/add', 'POST', patient)
 
-// // 编辑患者
-// export const editPatient = (patient: Patient) => request('patient/update', 'PUT', patient)
+// 编辑患者
+export const editPatient = (patient: Patient) => request('patient/update', 'PUT', patient)
 
 // 删除患者
 export const delPatient = (id: string) => request(`patient/del/${id}`, 'DELETE')
